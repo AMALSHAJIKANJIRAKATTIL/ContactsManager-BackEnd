8f07a7947fc7d0ab515b767d768af762a2686799
Pradyumna Thorat <thoratpradyumna@gmail.com>
2023-02-08 20:29:05 +0530

    create login api and jwt token

--- routes/ResisterAndLogin.js
+++ routes/ResisterAndLogin.js
@@ -6,14 +6,9 @@ router.use(express.json());
 const bcrypt = require('bcrypt');
 // const jwt = require("jsonwebtoken");
 const secret = "RESTAPI";
-
-router.post("/resister", body("email").isEmail(),
-    body("password").isLength({ min: 5, max: 12 }), async (req, res) => {
+const jwt = require("jsonwebtoken");
+router.post("/resister", async (req, res) => {
         try {
-            const error = validationResult(req);
-            if (!error.isEmpty()) {
-                return res.status(403).json({ error: "Please Enter Valid Email and Password" })
-            }
             const { email, password } = req.body;
             const user = await userModel.findOne({ email });
             if (user) {
@@ -41,6 +36,44 @@ router.post("/resister", body("email").isEmail(),
         }
 
     })
+
+router.post("/", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return res.status(403).json({
+                error: "Unkown User"
+            })
+        }
+        bcrypt.compare(password, user.password, function (err, result) {
+            if (err) {
+                return res.status(500).json({
+                    error: err.message
+                })
+            }
+            if (result) {
+                const token = jwt.sign({
+                    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                    data: user._id
+                }, secret);
+                return res.status(200).json({
+                    message: "Login successfull",
+                    token: token
+                })
+            } else {
+                return res.status(400).json({
+                    error: "Invallid Password"
+                })
+            }
+        })
+    } catch (err) {
+        return res.status(400).json({
+            status: "Failed",
+            message: err.message
+        })
+    }
+})
 
 
 module.exports = router;
